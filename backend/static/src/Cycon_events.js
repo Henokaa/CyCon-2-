af527053f72ea491b17e575fe074135829ee2935
Henokaa <69524541+Henokaa@users.noreply.github.com>
2024-05-17 16:42:42 -0600

    fix: images gallery

--- backend/static/src/Cycon_events.js
+++ backend/static/src/Cycon_events.js
@@ -2028,7 +2028,7 @@ function generatePDF(form) {
           pdf.setFontSize(10);
           pdf.setTextColor(150);
           // Add your footer content here
-          pdf.text("Copyright © CyCon 2024 version. Last updated: 05/1/2024", pdf.internal.pageSize.getWidth() - 100, pdf.internal.pageSize.getHeight() - 10);
+          pdf.text("Copyright © CyCon 2024 version. Last updated: 05/17/2024", pdf.internal.pageSize.getWidth() - 100, pdf.internal.pageSize.getHeight() - 10);
         }
         // Save the PDF with the footer
         pdf.save();
